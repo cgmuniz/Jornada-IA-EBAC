50b080a45b6a4024e9ea122f1618cd5f92387275
cgmuniz <cauamuniz00@gmail.com>
2024-04-13 20:37:46 -0300

    definir o que o player precisa fazer

--- controls.js
+++ controls.js
@@ -2,5 +2,9 @@ export default {
     jump: new KeyboardEvent('keydown', { key: ' ', code: 'Space' }),
     dispatch(event) {
         document.dispatchEvent(this[event])
-    }
+    },
+    crouch: new KeyboardEvent('keydown', { key: 'ArrowDown', code: 'ArrowDown' }),
+    dispatch(event) {
+        document.dispatchEvent(this[event])
+    },
 }
