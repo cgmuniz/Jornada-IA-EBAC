03be342b4b50ad3ce49013628f83ebbd0eaaf05b
cgmuniz <cauamuniz00@gmail.com>
2024-04-16 20:04:43 -0300

    controls update

--- controls.js
+++ controls.js
@@ -1,8 +1,5 @@
 export default {
     jump: new KeyboardEvent('keydown', { key: 'Space', keyCode: '32' }),
-    dispatch(event) {
-        document.dispatchEvent(this[event])
-    },
     crouch: new KeyboardEvent('keydown', { key: 'ArrowDown', keyCode: '40' }),
     dispatch(event) {
         document.dispatchEvent(this[event])
